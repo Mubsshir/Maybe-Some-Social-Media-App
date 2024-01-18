68c7a1b2785939d4043f8a5ac711ad1eaaf7fb6d
Mubasshir Khan <39831189+Mubsshir@users.noreply.github.com>
2024-01-19 01:04:08 +0530

    Custom datepicker styling

--- Frontend/frontpost/src/pages/Profile.jsx
+++ Frontend/frontpost/src/pages/Profile.jsx
@@ -1,14 +1,15 @@
 import { useState, useContext, useEffect } from "react"
-import { FaEdit, FaSave, FaLinkedin, FaInstagram, FaTwitch } from "react-icons/fa"
+import { FaEdit, FaSave, FaLinkedin, FaCalendar, FaInstagram, FaTwitch } from "react-icons/fa"
 import { GoX } from 'react-icons/go'
 import { AuthContext } from "../store/context"
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css";
-const options = { day: 'numeric', month: 'short', year: 'numeric' };
+import '../components/styles/datepicker.css'
+//const options = { day: 'numeric', month: 'short', year: 'numeric' };
 const Profile = () => {
   const [isEdit, setIsEdit] = useState(false);
   const { user } = useContext(AuthContext);
-  
+
   const userData = {
     Name: user[0].first_name + " " + user[0].last_name,
     DOB: new Date(user[0].dob),
@@ -18,8 +19,9 @@ const Profile = () => {
     bio: user[0].bio
   }
 
+
   const [profile, setProfile] = useState(userData);
-  const [startDate, setStartDate] = useState(userData['DOB']);
+  const [startDate, setStartDate] = useState(profile['DOB']);
   const onEditHandler = (e) => {
     e.preventDefault();
     if (isEdit) {
@@ -41,35 +43,63 @@ const Profile = () => {
 
     e.preventDefault();
     setProfile(userData)
-    console.log(profile)
+    setStartDate(userData['DOB'])
     setIsEdit(!isEdit)
   }
   useEffect(() => { }, [])
   const keysToRender = Object.keys(profile).filter((key) => key !== 'img' && key !== 'bio');
 
-  const info = keysToRender.map((key) => (
-    <div key={key} className="grid grid-cols-2 [&>*]:border-b [&>*]:border-gray-500 [&>*]:pb-2 [&>*]:text-lg mb-2">
-      <h6 className="">{key}</h6>
-      <input name="name" onChange={(e) => { onChangeHandler(e, key) }} value={key !== 'DOB' ? profile[key] : profile[key].toLocaleDateString('en-US', options)} className={` bg-transparent outline-none ${isEdit ? 'text-green-500 text' : ''}`} disabled={!isEdit} />
-    </div>
-  ))
+  const info = keysToRender.map((key) => {
+    if (key == 'Email') {
+      return (
+        <div key={key} className="grid grid-cols-2 [&>*]:border-b [&>*]:border-gray-500 [&>*]:pb-2 [&>*]:text-lg mb-2">
+          <label htmlFor={key}>{key}</label>
+          <input type="email" name={key} onChange={(e) => { onChangeHandler(e, key) }} value={profile[key]} className={`caret-transparent bg-transparent outline-none ${isEdit ? 'text-green-500 text' : ''}`} disabled={!isEdit} />
+        </div>
+      )
+    } else if (key == 'Phone') {
+      return (
+        <div key={key} className="grid grid-cols-2 [&>*]:border-b [&>*]:border-gray-500 [&>*]:pb-2 [&>*]:text-lg mb-2">
+          <label htmlFor={key}>{key}</label>
+          <input type="tel" name={key} maxLength={13} onChange={(e) => { onChangeHandler(e, key) }} value={profile[key]} className={`caret-transparent   bg-transparent outline-none ${isEdit ? 'text-green-500 text' : ''}`} disabled={!isEdit} />
+        </div>
+      )
+    } else if (key == 'DOB') {
+      return (
+        <div key={key} className="grid grid-cols-2 items-center mb-2 dobContainer">
+          <label htmlFor={key}>{key}</label>
+          <DatePicker
+            showIcon={isEdit}
+            name={key}
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            dateFormat="MMM d, y"
+            disabled={!isEdit}
+            wrapperClassName={`${isEdit ? "text-green-500" : ""}`}
+            icon={<FaCalendar className="text-green-500" />}
+          >
+            <h3 className="text-center font-bold text-green-500">So when did you born?</h3>
+          </DatePicker>
+          
+        </div>
+      )
+    }
+    else {
+      return (
+        <div key={key} className="grid grid-cols-2 [&>*]:border-b [&>*]:border-gray-500 [&>*]:pb-2 [&>*]:text-lg mb-2">
+          <label htmlFor={key}>{key}</label>
+          <input type="text" name={key} onChange={(e) => { onChangeHandler(e, key) }} value={profile[key]} className={`caret-transparent bg-transparent outline-none ${isEdit ? 'text-green-500 text' : ''}`} disabled={!isEdit} />
+        </div>
+      )
+    }
+  })
+  console.log(info)
   return (
     <main className="w-full h-full grid grid-cols-3 gap-2 bg-gradient-to-tr p-2 from-gray-700 to-black ">
       <section className="h-fit bg-gradient-to-t from-gray-800 to-black col-span-2 p-2 rounded-sm shadow-xl">
         <h3 className="text-green-500 text-4xl">About</h3>
         <form className=" mt-6 px-3">
           {info}
-
-          <div className="container mx-auto mt-8 bg-transparent text-black ">
-            <DatePicker
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-              dateFormat="MMM d, y"
-              className="border p-2"
-            />
-          </div>
-
-
           <div className="flex">
             <button onClick={onEditHandler} type={isEdit ? 'submit' : 'button'} className="mt-10 flex  text-lg justify-center items-center bg-transparent hover:bg-green-500 text-green-500 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded">
               {isEdit ? 'Save ' : 'Edit Information'}{isEdit && <FaEdit className="ml-2" /> && <FaSave className="ml-2" />}

bdb8eb915e9176d58f092112756f81669067d18a
Mubasshir Khan <39831189+Mubsshir@users.noreply.github.com>
2024-01-27 16:45:22 +0530

    Modal Rethink

--- Frontend/frontpost/src/components/ui/Modal.jsx
+++ Frontend/frontpost/src/components/ui/Modal.jsx
@@ -1,34 +1,43 @@
-
-import { useState } from 'react';
-import ReactDOM from 'react-dom';
+import { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types'
 
-const Backdrop = () => {
-  const [show, setShow] = useState(true);
-  const HideModal = () => {
-    setShow(false)
-  }
-  return (
-    show && <div onClick={HideModal} className='w-dvw h-dvh fixed bg-black bg-opacity-40 z-50' ></div>
-  )
-}
 
 const Modal = (props) => {
-  return (
+  const [isOpen, setIsOpen] = useState(true);
+  const ModalRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (ModalRef.current && !ModalRef.current.contains(event.target)) {
+      // Click occurred outside the Modal, close it
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    // Attach the event listener when the component mounts
+    document.addEventListener('mousedown', handleClickOutside);
+    // Clean up the event listener when the component unmounts
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return isOpen ? (
     <>
-      {ReactDOM.createPortal(<Backdrop />, document.getElementById("backdrop"))}
-      {ReactDOM.createPortal(
-        <div className={props.className}>
-          {props.children}
-        </div>, document.getElementById("modal")
-      )}
+      <div onClick={()=>{setIsOpen(false)}} className= 'w-dvw h-dvh bg-black bg-opacity-40 fixed left-0 top-0' ></div>
+      <div ref={ModalRef} className="w-1/3  top-1/3 bg-white fixed shadow-sm left-0" >
+        <div className='bg-green-500 px-1 py-1 font-semibold text-white'>
+          <h3>Error</h3>
+        </div>
+      </div>
     </>
-  )
-}
+  ) : null;
+};
+
 
 Modal.propTypes = {
   children: PropTypes.element.isRequired,
-  className: PropTypes.string.isRequired
-}
+  className: PropTypes.string
+};
 
 export default Modal;
--- Frontend/frontpost/src/pages/Auth.jsx
+++ Frontend/frontpost/src/pages/Auth.jsx
@@ -23,7 +23,7 @@ const Auth = () => {
   const passRef = useRef("");
   const cPassRef = useRef("");
 
-  const { setIsAuthenticated,setUID } = useContext(AuthContext);
+  const { setIsAuthenticated, setUID } = useContext(AuthContext);
 
   const formTypeHandler = () => {
     setIsSignIn(!signIn)
@@ -66,7 +66,7 @@ const Auth = () => {
     }
 
 
-    if (!signIn){
+    if (!signIn) {
       if (pass != cPass) {
         setErrorMsg("Password Not Matched..");
         setIsValid(false);
@@ -91,35 +91,36 @@ const Auth = () => {
     setShowSuccess(false);
   }, 5000);
   return (
-    <div className="xl:w-1/3 phone:w-11/12 phone:mx-0  md:w-4/5 sm:w-4/5 sm:mx-auto h-auto  bg-white bg-opacity-15 border border-white border-opacity-10 rounded-xl overflow-auto xl:mt-3.5 xl:mr-28 shadow-lg backdrop-blur-[10px] ">
-      <h3 className={headingClass}><span className="font-extrabold text-green-500 mr-2 font-mono">{signIn ? 'Login' : 'Sign Up'}</span> Here</h3>
-      {showSuccess && <h3 className="ml-4 text-white text-lg font-bold">Successfully Signed Up,You can <span className="text-green-500">Login</span> now</h3>}
-      <form className={formClass} onSubmit={onsubmitHandler} >
-        {!signIn && (
-          <div className="flex  items-stretch w-full [&>*]:w-[50%]">
-            <Input ref={firstRef} input={{ type: 'text', name: 'username', placeholder: 'First Name', maxLength: "12" }} labelName='First Name' />
-            <Input ref={lastRef} input={{ type: 'text', name: 'username', placeholder: 'Last Name', maxLength: "12" }} labelName='Last Name' />
+    <>
+      <div className=" xl:w-1/3 phone:w-11/12 phone:mx-0  md:w-4/5 sm:w-4/5 sm:mx-auto h-auto  bg-white bg-opacity-15 border border-white border-opacity-10 rounded-xl overflow-auto xl:mt-3.5 xl:mr-28 shadow-lg backdrop-blur-[10px] ">
+        <h3 className={headingClass}><span className="font-extrabold text-green-500 mr-2 font-mono">{signIn ? 'Login' : 'Sign Up'}</span> Here</h3>
+        {showSuccess && <h3 className="ml-4 text-white text-lg font-bold">Successfully Signed Up,You can <span className="text-green-500">Login</span> now</h3>}
+        <form className={formClass} onSubmit={onsubmitHandler} >
+          {!signIn && (
+            <div className="flex  items-stretch w-full [&>*]:w-[50%]">
+              <Input ref={firstRef} input={{ type: 'text', name: 'username', placeholder: 'First Name', maxLength: "12" }} labelName='First Name' />
+              <Input ref={lastRef} input={{ type: 'text', name: 'username', placeholder: 'Last Name', maxLength: "12" }} labelName='Last Name' />
+            </div>
+          )}
+          <Input ref={userRef} input={{ type: 'text', name: 'username', placeholder: 'username', maxLength: "12" }} labelName='Username' />
+          {!signIn && (
+            <div className="flex  items-stretch w-full [&>*]:w-[50%]">
+              <Input ref={emailRef} input={{ type: 'email', name: 'email', placeholder: 'Email' }} labelName='Email' />
+              <PickDate configuration={{}} labelName="DOB" fetchDate={getDob} />
+            </div>
+          )}
+          <Input ref={passRef} input={{ type: 'password', name: 'pass', placeholder: 'Password', suggested: "current-password" }} labelName='Password' />
+          {!signIn && <Input ref={cPassRef} input={{ type: 'password', name: 'cpass', placeholder: 'Confirm Password', maxLength: "12", suggested: "current-password" }} labelName='Confirm Password' />}
+          <div className="mx-1 w-full" >
+            <h3 className="text-lg text-white">{signIn ? "Don't" : 'Already'} have an Account <span className="cursor-pointer ml-1 font-bold text-green-400" onClick={formTypeHandler}  >{signIn ? '!Sign up here' : '!Sign in'}</span> </h3>
           </div>
-        )}
-        <Input ref={userRef} input={{ type: 'text', name: 'username', placeholder: 'username', maxLength: "12" }} labelName='Username' />
-        {!signIn && (
-          <div className="flex  items-stretch w-full [&>*]:w-[50%]">
-            <Input ref={emailRef} input={{ type: 'email', name: 'email', placeholder: 'Email' }} labelName='Email' />
-            <PickDate configuration={{}} labelName="DOB" fetchDate={getDob} />
-          </div>
-        )}
-        <Input ref={passRef} input={{ type: 'password', name: 'pass', placeholder: 'Password', suggested: "current-password" }} labelName='Password' />
-        {!signIn && <Input ref={cPassRef} input={{ type: 'password', name: 'cpass', placeholder: 'Confirm Password', maxLength: "12", suggested: "current-password" }} labelName='Confirm Password' />}
-        <div className="mx-1 w-full" >
-          <h3 className="text-lg text-white">{signIn ? "Don't" : 'Already'} have an Account <span className="cursor-pointer ml-1 font-bold text-green-400" onClick={formTypeHandler}  >{signIn ? '!Sign up here' : '!Sign in'}</span> </h3>
-        </div>
-        <Input input={{ type: 'submit', value: signIn ? 'Login' : 'Create Account' }} className="bg-green-500  cursor-pointer mt-4  py-2 text-xl  text-white rounded-lg" />
-      </form>
-      <Modal>
-        <div>Hello</div>
-      </Modal>
-      {!isValid && <p className="text-red-600 font-semibold text-xl px-4 mb-3 ml-2">{ErrorMsg}</p>}
-    </div >
+          <Input input={{ type: 'submit', value: signIn ? 'Login' : 'Create Account' }} className="bg-green-500  cursor-pointer mt-4  py-2 text-xl  text-white rounded-lg" />
+        </form>
+
+        {!isValid && <p className="text-red-600 font-semibold text-xl px-4 mb-3 ml-2">{ErrorMsg}</p>}
+      </div >
+      <Modal className="bg-white z-10"/>
+    </>
   )
 }
 
